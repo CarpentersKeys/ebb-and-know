f079b4c8a557b1880f89133b459a2b16c1b6e012
joshua <carpenterskeys@gmail.com>
2022-05-03 20:45:21 -0400

    override _document.js default to bring in new fonts (zen maru and league spartan), modified document flow to demo.js and conditionally rendered DisaplyContent component, reverted index.js and fixed a tags by replacing with next/Link components

--- pages/demo.js
+++ pages/demo.js
@@ -1,14 +1,16 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Entry from '../components/Entry';
 import Prompt from '../components/Prompt';
 import Schedule from '../components/Schedule';
 import TimelinePreview from '../components/TimelinePreview';
 import PendingTab from '../components/PendingTab';
 import styles from '../styles/demo.module.scss'
+import DisplayContent from '../components/DisplayContent';
 
 export default function AppDemo() {
     const [reviewItems, reviewItemsSet] = useState([]);
     const [pendingReviews, pendingReviewsSet] = useState([]);
+    const [selectedReview, selectedReviewSet] = useState(null);
 
     // useEffect(() => {
     //     console.log('pend:', pendingReviews)
@@ -18,12 +20,12 @@ export default function AppDemo() {
     return (
         <div className={styles.body}>
             <h1>Ebb..</h1>
-            <Prompt count={reviewItems.length} />
-            <Entry reviewItems={reviewItems} reviewItemsSet={reviewItemsSet} />
             <Schedule reviewItems={reviewItems} reviewItemsSet={reviewItemsSet} pendingReviewsSet={pendingReviewsSet} />
             <TimelinePreview reviewItems={reviewItems} />
-            {/* CURRENT: display reviews */}
-            <PendingTab pendingReviews={pendingReviews} pendingReviewsSet={pendingReviewsSet} />
+            <Prompt count={reviewItems.length} />
+            {!selectedReview && <Entry reviewItems={reviewItems} reviewItemsSet={reviewItemsSet} />}
+            {selectedReview && <DisplayContent selectedReview={selectedReview} />}
+            <PendingTab selectedReviewSet={selectedReviewSet} pendingReviews={pendingReviews} pendingReviewsSet={pendingReviewsSet} />
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,75 +1,73 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
-import { Flex, Heading } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <Flex height='100vh' alignItems='center' justifyContent='center'>
 
-      <Flex direction='column'><Heading>aewfaewf</Heading></Flex>
-      {/* <div className={styles.container}>
-        <Head>
-          <title>Ebb and Know</title>
-          <meta name="description" content="a simple review protocal based on the Ebbinghaus forgetting curve" />
-          <link rel="icon" href="" />
-        </Head>
+    <div className={styles.container}>
+      <Head>
+        <title>Ebb and Know</title>
+        <meta name="description" content="a simple review protocal based on the Ebbinghaus forgetting curve" />
+        <link rel="icon" href="" />
+      </Head>
 
-        <main className={styles.main}>
-          <h1 className={styles.title}>
-            Welcome!.. to <a href="http://localhost:3000/">Ebb and Know</a>
-          </h1>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome!.. to <a href="http://localhost:3000/">Ebb and Know</a>
+        </h1>
 
-          <p className={styles.description}>
-            a simple review protocal{' '}
-            <code className={styles.code}>based on the Ebbinghaus forgetting curve</code>
-          </p>
+        <p className={styles.description}>
+          a simple review protocal based on the{' '}
+          <code className={styles.code}> Ebbinghaus forgetting curve</code>
+        </p>
 
-          <div className={styles.grid}>
+        <div className={styles.grid}>
+          <div className={styles.card}>
+            <Link passHref href="/user" className={styles.link}>
+              <h2>Log in &rarr;</h2>
+            </Link>
+            <p>or</p>
+            <Link passHref href="/user" className={styles.link}>
+              <h2>create an account &rarr;</h2>
+            </Link>
+
+          </div>
+
+          <Link passHref href="/demo" >
             <div className={styles.card}>
-              <a href="/user" className={styles.link}>
-                <h2>Log in &rarr;</h2>
-              </a>
-              <p>or</p>
-              <a href="/user" className={styles.link}>
-                <h2>create an account &rarr;</h2>
-              </a>
-
+              <h2>See what we&apos;ve done &rarr;</h2>
+              <p>check out how we implement Ebbinghaus&apos; ideas.</p>
             </div>
+          </Link>
+        </div>
 
-            <a href="/demo" className={styles.card}>
-              <h2>See what we've done &rarr;</h2>
-              <p>check out how we implement Ebbinghaus' ideas.</p>
-            </a>
-          </div>
-
-          <div className={styles.grid}>
-            <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4492928/" className={styles.card}>
-              <h2>Science &rarr;</h2>
-              <p>Learn about the principles</p>
-            </a>
-
-            <a href="https://getpocket.com/explore/item/train-your-brain-to-remember-anything-you-learn-with-this-simple-20-minute-habit" className={styles.card}>
-              <h2>Read &rarr;</h2>
-              <p>Something a little lighter</p>
-            </a>
-          </div>
-        </main >
-
-        <footer className={styles.footer}>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{' '}
-            <span className={styles.logo}>
-              <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-            </span>
+        <div className={styles.grid}>
+          <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4492928/" className={styles.card}>
+            <h2>Science &rarr;</h2>
+            <p>Learn about the principles</p>
           </a>
-        </footer>
-      </div > */}
-      aewfaewfa
-    </Flex>
+
+          <a href="https://getpocket.com/explore/item/train-your-brain-to-remember-anything-you-learn-with-this-simple-20-minute-habit" className={styles.card}>
+            <h2>Read &rarr;</h2>
+            <p>Something a little lighter</p>
+          </a>
+        </div>
+      </main >
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div >
   )
 }
